118f615ebd9c818a671504da6d17114fecae46ea
Win Bunyasrie <paveen.bunyasrie@gmail.com>
2022-01-12 10:07:04 -0500

    Adding PyPoll.py file

--- PyPoll.py
+++ PyPoll.py
@@ -8,19 +8,42 @@
 
 import csv
 import os
-# Assign a variable to load a file from a path.
+
 file_to_load = os.path.join("Resources", "election_results.csv")
-# Assign a variable to save the file to a path.
+
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Open the election results and read the file.
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
 with open(file_to_load) as election_data:
     
-    # Read the file object with the reader function
+  
     file_reader = csv.reader(election_data)
     
-    # Read and print the header row.
+  
     headers = next(file_reader)
-    print(headers)
+    
+
+    
+    for row in file_reader:
+        total_votes += 1
+
+        candidate_name = row[2]
+        
+        if candidate_name not in candidate_options:
+            
+            candidate_options.append(candidate_name)
+            
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+    
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = float(votes) / float(total_votes) * 100
+    
+    print(f"{candidate_name}: {vote_percentage}% of the vote.")
+
 
 
